84b795b5fd3cf1d9492a82206867400f6184ef21
Matt Antonelli <contact@raelys.com>
2023-10-10 10:24:41 -0400

    Migrate card styles to CSS module, remove image
    Card images will be fetched directly from ATTT to automatically keep
    the images consistent with the API data after an update.

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import styles from './card.module.scss';
 
 export default function Card({ card }) {
   const stats = Object.values(card.stats.formatted).join(' ');
@@ -6,7 +7,7 @@ export default function Card({ card }) {
   return (
     <div className="m-1">
       <img src="/images/blank.png" alt={`${card.name} (${card.stats})`} title={card.name}
-        className="card-large" style={{backgroundPosition: `-${(card.id - 1) * 104}px 0`}} />
+        className={styles.card} style={{backgroundPosition: `-${(card.id - 1) * 104}px 0`}} />
     </div>
   )
 }
