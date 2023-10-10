b555fb37c7d2b86e4de5365ae265e2536f139538
Matt Antonelli <contact@raelys.com>
2023-10-10 12:28:51 -0400

    Add margin directly to Card, fix alt text

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,9 +5,7 @@ export default function Card({ card }) {
   const stats = Object.values(card.stats.formatted).join(' ');
 
   return (
-    <div className="m-1">
-      <img src="/images/blank.png" alt={`${card.name} (${card.stats})`} title={card.name}
-        className={styles.card} style={{backgroundPosition: `-${(card.id - 1) * 104}px 0`}} />
-    </div>
+    <img src="/images/blank.png" alt={`${card.name} (${stats})`} title={card.name}
+      className={styles.card} style={{backgroundPosition: `-${(card.id - 1) * 104}px 0`}} />
   )
 }
