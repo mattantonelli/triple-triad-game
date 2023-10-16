79c502b55838afedcc688e105f7e4e77b71943c5
Matt Antonelli <contact@raelys.com>
2023-10-16 13:35:43 -0400

    Add play instructions

--- src/app/play/page.jsx
+++ src/app/play/page.jsx
@@ -6,6 +6,20 @@ export default async function Play() {
   const cards = await getCardsById();
 
   return (
-    <Game cards={cards} decks={decks} />
+    <div className="d-flex flex-column">
+      <Game cards={cards} decks={decks} />
+      <div className="card mx-auto mt-3">
+        <h5 className="card-header">How to play</h5>
+        <div className="card-body pb-0">
+          <ul>
+            <li>Select a deck for each player.</li>
+            <li>Players take turns placing their cards on the board.</li>
+            <li>Flip your opponent&apos;s cards by playing a higher value on an adjacent side.</li>
+            <li>The player with the most flipped cards wins!</li>
+            <li>Click anywhere on the board to start a new game.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
