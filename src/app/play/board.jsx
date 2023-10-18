4ae1e781d997f62b0ed3fefc1fe967d163fe009f
Matt Antonelli <contact@raelys.com>
2023-10-18 12:05:44 -0400

    Add card flipping
    Updates the Board to track its previous state so it can determine
    which squares were flipped. Adds a flip animation to the card.

--- src/app/play/board.jsx
+++ src/app/play/board.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useRef } from "react";
 import Square from "./square";
 import styles from "./styles.module.scss";
 
 export default function Board({ squares, playCard }) {
+  const previousSquares = useRef(Array(9).fill({}));
+
+  // Track the squares from the previous render so we can see which ones were flipped
+  useEffect(() => {
+    // We need to create a deep copy since squares is an array of objects
+    previousSquares.current = squares.map((square) => ({...square}));
+  });
+
+  // Returns true if a square occupied in the last render had a different color
+  function isFlipped(i) {
+    return previousSquares.current[i].color &&
+      (squares[i].color !== previousSquares.current[i].color);
+  }
+
   return (
     <div className={styles.board}>
       <div className={`d-flex flex-wrap ${styles.playArea}`}>
         {squares.map((square, i) => {
-          return <Square key={i} index={i} card={square.card} color={square.color} playCard={playCard} />;
+          return <Square key={i} index={i} card={square.card} color={square.color}
+            flipped={isFlipped(i)} playCard={playCard} />;
         })}
       </div>
     </div>
--- src/app/play/square.jsx
+++ src/app/play/square.jsx
@@ -2,7 +2,7 @@ import { useDrop } from "react-dnd";
 import PlayCard from "./playCard";
 import styles from "./styles.module.scss";
 
-export default function Square({ index, card, color, playCard }) {
+export default function Square({ index, card, color, flipped, playCard }) {
   function placeCard(placedCard, color) {
     playCard(placedCard, color, index);
   }
@@ -21,7 +21,7 @@ export default function Square({ index, card, color, playCard }) {
   }), [index, card, color, playCard]);
 
   return (
-    <div className={styles.playSquare} ref={dropRef}
+    <div className={`${styles.playSquare} ${flipped ? styles[`${color}Flip`] : ""}`} ref={dropRef}
       style={{backgroundColor: isOver && isEmpty() ? "rgba(0, 0, 0, 0.3)" : "rgba(0, 0, 0, 0.1)"}}>
       {card && <PlayCard card={card} color={color} isDraggable={false} isVisible={true} />}
     </div>
