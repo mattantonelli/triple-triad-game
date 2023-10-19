a9c9adab09eb23ebe16cbe039170bbc40ff3b1ca
Matt Antonelli <contact@raelys.com>
2023-10-19 19:29:08 -0400

    First pass at preloading Play images

--- src/app/play/page.jsx
+++ src/app/play/page.jsx
@@ -6,20 +6,31 @@ export default async function Play() {
   const cards = await getCardsById();
 
   return (
-    <div className="d-flex flex-column">
-      <Game cards={cards} decks={decks} />
-      <div className="card mx-auto mt-3">
-        <h5 className="card-header">How to play</h5>
-        <div className="card-body pb-0">
-          <ul>
-            <li>Select a deck for each player.</li>
-            <li>Players take turns placing their cards on the board.</li>
-            <li>Flip your opponent&apos;s cards by playing a higher value on an adjacent side.</li>
-            <li>The player with the most flipped cards wins!</li>
-            <li>Click anywhere on the board to start a new game.</li>
-          </ul>
+    <>
+      {["red", "blue"].map((color) => {
+        return (
+          <>
+            <link rel="preload" as="image" href={`https://triad.raelys.com/images/cards/large_${color}.png`} />
+            <link rel="preload" as="image" href={`/images/messages/turns/${color}_turn.png`} />
+          </>
+        );
+      })}
+
+      <div className="d-flex flex-column">
+        <Game cards={cards} decks={decks} />
+        <div className="card mx-auto mt-3">
+          <h5 className="card-header">How to play</h5>
+          <div className="card-body pb-0">
+            <ul>
+              <li>Select a deck for each player.</li>
+              <li>Players take turns placing their cards on the board.</li>
+              <li>Flip your opponent&apos;s cards by playing a higher value on an adjacent side.</li>
+              <li>The player with the most flipped cards wins!</li>
+              <li>Click anywhere on the board to start a new game.</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
