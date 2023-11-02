5b57f01ae949170b1edcbd1f57bcfea4ae8acffc
Matt Antonelli <contact@raelys.com>
2023-11-02 10:15:12 -0400

    Improve clarity of default value for Rule select

--- src/app/play/rules.jsx
+++ src/app/play/rules.jsx
@@ -8,7 +8,7 @@ export default function Rules({ rule, selectRule, isPlayStarted }) {
  return (
   <div className={`d-flex flex-column ${styles.rules}`}>
     <select className="form-select" value={rule} onChange={e => selectRule(e.target.value)} disabled={isPlayStarted()}>
-      <option value="">Select a rule</option>
+      <option value="">No Rule</option>
       {rules.map((val, i) => {
         return <option key={i} value={val}>{val}</option>;
       })}
